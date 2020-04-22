f1d5a5acfdf049913b9928f07d03cf64a76392bd
Bruno Gonçalves <brunomgoncalves@gmail.com>
2020-04-22 18:54:11 -0300

    Tela Login

---
+++ src/JSE.Web/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSE.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
