fcbad446d51c60c99952c120a119123b863100e3
Bruno Gonçalves <brunomgoncalves@gmail.com>
2020-12-26 14:04:52 -0300

    Footer

---
+++ src/JSE.Web/Extensions/Component/FooterViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using JSE.Web.Repositories.Intefarces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace JSE.Web.Extensions.Component
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly IEstabelecimentoRepository _repo;
+
+        public FooterViewComponent(IEstabelecimentoRepository repository)
+        {
+            _repo = repository;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var estab = _repo.ObterEstabelecimento();
+            return View(estab);
+        }
+    }
+}
